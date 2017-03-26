76229cd939f6c06db4b4cd9454ae9f3ea212305c
Wade Novak <wade@launchpadlab.com>
2017-03-26 14:31:35 -0500

    WIP homepage

---
+++ src/homepage/homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello World! Welcome to the Hit the Books App!</h2>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
