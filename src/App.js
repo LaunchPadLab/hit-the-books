d119777dedd2898bad7f7a6cb7a076fe2f9a2684
Rachel Killackey <rachel@launchpadlab.com>
2017-04-04 12:00:55 -0500

    use BrowserRouter, update homepage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
-import { Route, HashRouter } from 'react-router-dom';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, BrowserRouter, Link } from 'react-router-dom'
+import './App.css'
 import Homepage from './pages/homepage.js'
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <Route path="/" component={ Homepage } />
-      </HashRouter>
-    );
+      <BrowserRouter>
+        <div>
+          <header>
+            <a href="/">Home</a>
+            <nav>
+              <ul>
+                <li><Link to="/all-books">View Books</Link></li>
+                <li><Link to="/sell-books">Sell Books</Link></li>
+              </ul>
+            </nav>
+          </header>
+          <Route exact path="/" component={ Homepage }/>
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
-export default App;
+export default App
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,23 +1,14 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Homepage extends Component {
   render() {
     return (
       <div>
-        <header>
-          <a href="/">Home</a>
-          <nav>
-            <ul>
-              <li><Link to="/all-books">View Books</Link></li>
-              <li><Link to="/sell-books">Sell Books</Link></li>
-            </ul>
-          </nav>
-        </header>
         <h2>Hello World! Welcome to the Hit the Books App!</h2>
       </div>
-    );
+    )
   }
 }
 
-export default Homepage;
+export default Homepage
