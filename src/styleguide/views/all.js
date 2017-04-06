92055717496bf4515fb872734c9b33d350a96097
Monique Marchwiany <monique@launchpadlab.com>
2017-04-06 15:53:03 -0500

    styles

--- src/styleguide/views/all.js
+++ src/styleguide/views/all.js
@@ -3,77 +3,124 @@ import React from 'react'
 function All () {
   return (
     <div>
-      <h2>Welcome to the styleguide!</h2>
-
       <header className="navigation">
-        <a href="/" className="logo link-white-base">Hit the Books</a>
-        <nav>
-         <ul>
-           <li><a href="#">Buy Books</a></li>
-           <li><a href="#">Sell Books</a></li>
-         </ul>
-        </nav>
+        <a href="/" className="logo link-white-base">Styleguide</a>
       </header>
-      <section>
-        <main className="content">
-          <div className="header-container">
-            <div className="card">
-              <h2>Understanding Art <span>(10th Edition)</span></h2>
-              <p>Authors: Lois Fichner-Rathus</p>
-              <a href="#" className="button-primary">Contact Seller</a>
+
+      <nav className="side-navigation">
+        <h5>Styleguide</h5>
+        <ul>
+          <li><a href="#typography">Typography</a></li>
+          <li><a href="#form-inputs">Form Inputs</a></li>
+          <li><a href="#buttons">Buttons</a></li>
+          <li><a href="#nav-bar">Nav Bar</a></li>
+          <li><a href="#card">Card</a></li>
+          <li><a href="#grid">Grid</a></li>
+        </ul>
+      </nav>
+
+      <main className="content with-sidebar styleguide">
+
+        <div className="content-block-container">
+          <h3 className="code-header" id="typography">Typography</h3>
+          <div className="card">
+            <h1>H1 - Header One</h1>
+            <h2>H2 - Header Two</h2>
+            <p>p — Some paragraph text</p>
+            <a href="#">a — Some link text</a>
+            <xmp><u>Underline text</u></xmp>
+          </div>
+
+          <h3 className="code-header" id="form-inputs">Form Inputs</h3>
+            <h5>Input</h5>
+            <div className="card styleguide-fields">
+              <fieldset>
+                <label>Label</label>
+                <input type="text" name="" placeholder="Placeholder Text" />
+              </fieldset>
+              <fieldset className="error">
+                <label>Label</label>
+                <input type="text" name="" placeholder="Placeholder Text" />
+                <span className="error-message">Label can't be blank</span>
+              </fieldset>
             </div>
+
+            <h5>Textarea</h5>
+            <div className="card styleguide-fields">
+              <fieldset>
+                <label>Label</label>
+                <textarea name="message" placeholder="Placeholder Text"></textarea>
+              </fieldset>
+
+              <fieldset className="error">
+                <label>Label</label>
+                <textarea name="message" placeholder="Placeholder Text"></textarea>
+                <span className="error-message">Label can't be blank</span>
+              </fieldset>
+            </div>
+
+            <h3 className="code-header" id="buttons">Buttons</h3>
+            <div className="card">
+              <button className="button-primary">Submit</button>
+              <button className="button-primary in-progress">Submit</button>
+              <button className="button-primary is-disabled">Submit</button>
+              <xmp>button-primary
+              button-primary in-progress
+              button-primary is-disabled
+              </xmp>
+            </div>
+
+            <h3 className="code-header" id="buttons">Nav Bar</h3>
+            <div className="card styleguide-header">
+              <header className="navigation">
+               <div className="content-block-container">
+                 <a href="/" className="logo link-white-base">Hit the Books</a>
+                 <nav>
+                   <ul>
+                    <li><a href="#" className="is-active">Buy Books</a></li>
+                    <li><a href="#">Sell Books</a></li>
+                   </ul>
+                 </nav>
+               </div>
+              </header>
+            </div>
+                <h3 className="code-header" id="grid">Grid</h3>
+                <div className="card">
+                  <div className="row">
+                    <div className="col-1">Test</div>
+                    <div className="col-5">Test</div>
+                    <div className="col-6">Test</div>
+                  </div>
+            <xmp>col-1
+            col-2
+            col-3
+            col-4
+            col-5
+            col-6
+            col-7
+            col-8
+            col-9
+            col-10
+            col-11
+            col-12
+            </xmp>
+                </div>
+
+            <h3 className="code-header" id="card">Card</h3>
+            <div className="card">
+              <div className="book-list">
+                <div className="card book-preview">
+                 <div className="text-block">
+                   <h2>Understanding Art <span>(10th Edition)</span></h2>
+                   <p>Authors: Lois Fichner-Rathus</p>
+                 </div>
+                 <a href="#" className="button-primary">Contact Seller</a>
+                </div>
+              </div>
+            </div>
+
           </div>
         </main>
-      </section>
-      <main className="content centered">
-        <section>
-          <div className="card">
-            <h2>Sell a Book</h2>
-            <fieldset>
-              <label>Title</label>
-              <input type="text" name="" placeholder="Placeholder Text" />
-            </fieldset>
-            <fieldset>
-              <label>Authors</label>
-              <input type="text" name="" placeholder="Placeholder Text" />
-            </fieldset>
-            <div className="row">
-              <fieldset className="col-6">
-                <label>Author</label>
-                <input type="text" name="" placeholder="Placeholder Text" />
-              </fieldset>
-              <fieldset className="col-6">
-                <label>Course</label>
-                <input type="text" name="" placeholder="Placeholder Text" />
-              </fieldset>
-            </div>
-            <fieldset>
-              <label>Book Description</label>
-              <textarea name="message" placeholder="What's this book about..."></textarea>
-            </fieldset>
-            <button className="button-primary">Place Book for Sale</button>
-          </div>
-        </section>
-      </main>
-      <main className="content">
-        <section>
-          <a href="#">Back to Search</a>
-          <div className="card">
-            <h2>Understanding Art <span>(10th Edition)</span></h2>
-            <table>
-              <tr>
-              </tr>
-
-            </table>
-          </div>
-
-          <div className="card">
-            <h2>Interested?</h2>
-            <p>Negotiate your price and get the book you need fast.</p>
-            <a href="#" className="button-primary">Contact Seller</a>
-          </div>
-        </section>
-      </main>
     </div>
   )
 }
--- src/styleguide/views/sandbox.js
+++ src/styleguide/views/sandbox.js
@@ -3,7 +3,135 @@ import React from 'react'
 function Sandbox () {
   return (
     <section>
-      <h2>Welcome to the sandbox!</h2>
+      <header className="navigation">
+        <div className="content-block-container">
+          <a href="/" className="logo link-white-base">Hit the Books</a>
+          <nav>
+            <ul>
+             <li><a href="#" className="is-active">Buy Books</a></li>
+             <li><a href="#">Sell Books</a></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+
+      <main className="content">
+        <div className="search-block">
+          <div className="content-block-container">
+            <label>SEARCH</label>
+            <input type="text" name="" placeholder="What are you looking for?" />
+          </div>
+        </div>
+        <div className="content-block-container">
+          <p><strong>Recently Added Books</strong></p>
+          <div className="book-list">
+            <div className="card book-preview">
+              <div className="text-block">
+                <h2>Understanding Art <span>(10th Edition)</span></h2>
+                <p>Authors: Lois Fichner-Rathus</p>
+              </div>
+              <a href="#" className="button-primary">Contact Seller</a>
+            </div>
+            <div className="card book-preview">
+              <div className="text-block">
+                <h2>Understanding Art <span>(10th Edition)</span></h2>
+                <p>Authors: Lois Fichner-Rathus</p>
+              </div>
+              <a href="#" className="button-primary">Contact Seller</a>
+            </div>
+            <div className="card book-preview">
+              <div className="text-block">
+                <h2>Understanding Art <span>(10th Edition)</span></h2>
+                <p>Authors: Lois Fichner-Rathus</p>
+              </div>
+              <a href="#" className="button-primary">Contact Seller</a>
+            </div>
+            <div className="card book-preview">
+              <div className="text-block">
+                <h2>The Reading Strategies Book <span>(10th Edition)</span></h2>
+                <p>Authors: Lois Fichner-Rathus</p>
+              </div>
+              <a href="#" className="button-primary">Contact Seller</a>
+            </div>
+          </div>
+        </div>
+      </main>
+
+
+      <main className="content centered">
+        <section>
+          <div className="card">
+            <h2>Sell a Book</h2>
+            <fieldset>
+              <label>Title</label>
+              <input type="text" name=""/>
+            </fieldset>
+            <fieldset>
+              <label>Book Edition</label>
+              <input type="text" name=""/>
+            </fieldset>
+            <fieldset>
+              <label>Authors</label>
+              <input type="text" name="" />
+            </fieldset>
+            <div className="row">
+              <fieldset className="col-6">
+                <label>ISBN</label>
+                <input type="text" name=""/>
+              </fieldset>
+              <fieldset className="col-6">
+                <label>Course</label>
+                <input type="text" name="" />
+              </fieldset>
+            </div>
+            <fieldset>
+              <label>Book Description</label>
+              <textarea name="message" placeholder="What's this book about..."></textarea>
+            </fieldset>
+            <div className="button-area">
+              <button className="button-primary">Place Book for Sale</button>
+            </div>
+          </div>
+        </section>
+      </main>
+
+
+      <main className="content">
+        <div className="content-block-container">
+          <a href="#" className="back-link">Back to Search</a>
+          <section className="row">
+            <div className="col-8">
+              <div className="card">
+                <h2>Understanding Art <span>(10th Edition)</span></h2>
+                <table>
+                  <tr>
+                    <td>Authors:</td>
+                    <td>Saundra K Ciccarelli, J Noland White</td>
+                  </tr>
+                  <tr>
+                    <td>ISBN:</td>
+                    <td>0205972241</td>
+                  </tr>
+                  <tr>
+                    <td>Course:</td>
+                    <td>PSYCH 100, PSYCH 101</td>
+                  </tr>
+                  <tr>
+                    <td>Decription:</td>
+                    <td>David Myers’ new partnership with coauthor C. Nathan DeWall matches two dedicated educators and scholars, each passionate about teaching psychological science through writing and interactive media. With this new edition of the #1 bestselling Psychology, Myers and DeWall take full advantage of what an integrated text/media learning combination can do. New features move students from reading the chapter to actively learning online: How Would You Know puts students in the role of scientific researcher and includes tutorials on key research design principles; Assess Your Strengths self-tests help students learn a little more about themselves, and include tips about nurturing key strengths. These and other innovations rest on the same foundations that have always distinguished a new David Myers edition—exhaustive updating (hundreds of new citations), captivating writing, and the merging of rigorous science with a broad human perspective that engages both the mind and heart. This edition of Psychology is accompanied by its own dedicated version of Worth Publishers’ breakthrough online course space, featuring a fully integrated e-Book, LearningCurve adaptive quizzing, a rich collection of student media resources, and easy setup, assessment, and course management options for instructors.</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            <div className="card col-4 interested">
+              <h2>Interested?</h2>
+              <p>Negotiate your price and get the book you need fast.</p>
+              <a href="#" className="button-primary col-12">Contact Seller</a>
+            </div>
+          </section>
+        </div>
+      </main>
     </section>
   )
 }
